98cbd2e1fc79bc711659d281d8a998ecc5a14c21
attesu <atte.sulander@hotmail.com>
2025-03-25 10:34:40 +0200

    Komponentti
    Jaettu omiin komponentteihin ja tuotu ne App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,18 @@
+import { useState } from "react";
+import ResultList from "./ResultList";
+import Header from "./Header";
+import TextInput from "./TextInput";
+
 function App() {
-
-  return (
+return (
+   
 <div>
-  <h1>MuistiinpanoApp</h1>
-  <div>
-    <button>Luo muistiinpano</button>
-    <button>listaa muistiinapanot</button>
-    <button>lisää kurssi</button>
-    <h4>Lisää uusi muistiinpano</h4>
-    <textarea name="textinput" id=""></textarea>
-    <br />
-    <button>tallenna</button>
-    <button>takaisin</button>
-  </div>
-  <br />
-
-
-
-  <div>
-  <h4>kurssit: (alasvetovalikko)</h4>
-    <button>takaisin</button>
-  <div style={{ border: "1px solid black"}}>
-  
-  <p>21.9.2022 Javascript (id 0)</p>
-  <span>Thänä kohtaan tulee tallennettu teksti</span>
-  </div>
-  <div style={{ border: "1px solid black"}}>
-  
-  <p>21.9.2022 Javascript (id 0)</p>
-  <span>Thänä kohtaan tulee tallennettu teksti</span>
-  </div>
-  <div style={{ border: "1px solid black"}}>
-  
-  <p>21.9.2022 Javascript (id 0)</p>
-  <span>Thänä kohtaan tulee tallennettu teksti</span>
-  </div>
-
-  </div>
-<br />
-  <div>
-  <button>lisää kurssi</button>
-  <button>takaisin</button>
-  <div>
-    <textarea name="coursename" id=""></textarea>
-  </div>
-  </div>
-
+  <Header />
+  <TextInput />
+  <ResultList />
 </div>
 
-  )
+  );
 }
 
 export default App
