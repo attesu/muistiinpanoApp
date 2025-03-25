11a77bc1a06c13ed8a37fe840755d4b2dc81b0f8
attesu <atte.sulander@hotmail.com>
2025-03-25 10:36:41 +0200

    Lista
    Luotu lista tallennetuista muistiinpanoista.

---
+++ src/ResultList.jsx
@@ -0,0 +1,16 @@
+import ResultRow from "./ResultRow";
+
+function ResultList(){
+    return(
+        <div>
+  <h4>kurssit: (alasvetovalikko)</h4>
+    <button>takaisin</button>
+    <ResultRow />
+    <ResultRow />
+    <ResultRow />
+
+  </div>
+    )
+}
+
+export default ResultList;
