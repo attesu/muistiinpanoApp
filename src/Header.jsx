6ba5f225bc7fdbc00e8f27dc3477f7f99e5203fd
attesu <atte.sulander@hotmail.com>
2025-03-25 10:36:01 +0200

    Header
    Headeri luotu

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return <div> <h1>MuistiinpanoApp</h1>
+    <button>Luo muistiinpano</button>
+    <button>Listaa muistiinpano</button>
+    <button>Lisää kurssi</button>
+    
+    </div>
+  
+    }
+
+    export default Header;
